8231b131c9c7595ffd3ea9f5d6d6263dedcaecf0
zuhra <zuhraaldaiarova@icloud.com>
2025-10-28 23:10:41 +0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,12 @@
+function login (){
+    let password=prompt("сыр созду жазыныз!")
+    switch (password) {
+        case"12345":
+        alert("туура,кош келиниз!")
+        break;
+    default:
+        alert("сыр соз туура эмес!")
+        break;
+}
+}
+login()
